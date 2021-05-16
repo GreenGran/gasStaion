b9c24ea6ecf225a3f2fef096aa09ffc962d75ddd
john almozlino <johnalmozlino@gmail.com]>
2021-05-16 18:42:28 +0300

    testing git hub

--- gasStaion_Csharp/gasStaion_Csharp/Program.cs
+++ gasStaion_Csharp/gasStaion_Csharp/Program.cs
@@ -6,7 +6,7 @@ namespace gasStaion_Csharp
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("git good");
         }
     }
 }
